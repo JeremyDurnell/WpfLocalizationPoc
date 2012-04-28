d6e6fde4f6f6eeb2cdae8984ac0e5f35aac9ce76
Jeremy Durnell <jeremy@durnell.me>
2012-04-28 13:53:45 -0500

    added pseudo-localization

--- GenerateFR.bat
+++ GenerateFR.bat
@@ -1,5 +1,7 @@
 SET locbaml="%~dp0\tools\locbaml.exe"
 
+powershell.exe ".\Create-PseudoLocalization.ps1 .\WpfApplication1\bin\Release\en-US.csv .\fr.csv"
+
 copy "%~dp0\fr.csv" "%~dp0\WpfApplication1\bin\Release" /y
 
 pushd "%~dp0\WpfApplication1\bin\Release"
